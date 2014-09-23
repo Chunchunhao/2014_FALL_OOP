c68d95ab995aa8b17d01ef6a353b405fbc57c02a
oldwang <b00902126@ntu.edu.tw>
2014-09-23 23:09:36 +0800

    Something i forgot

---
+++ week1/Code20140916/AddSum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	int num;
+	cout << "Enter the number to be summed: ";
+	while (cin >> num) {
+		sum += num;
+		cout << "Enter the number to be summed (non-integer to quit): ";
+	}
+	cout << "Sum is: " << sum << endl;
+	return 0;
+}
